9c5640433a7cc0c3657de4219d0520669264241b
demo1207 <100425282+demo1207@users.noreply.github.com>
2023-07-19 15:07:10 +0700

    update local beam

--- src/local_beam.py
+++ src/local_beam.py
@@ -1,59 +1,101 @@
 from read_data import *
 import itertools
+import numpy as np
 
-def LocalBeam(k: int, max_inter=100):   
-    def evaluate(solution):
-        total_value = 0
-        total_weight = 0
+def evaluateV(solution):
+    totalV = 0
+    totalW = 0
+    for i in range(n):
+        if solution[i] == 1:
+            totalV += v[i]
+            totalW += w[i]
+    if totalW > W:
+        totalV = 0
+
+    return totalV #class
+
+def generate_random(size = n):
+    state = np.random.randint(2, size=size)
+    return ''.join(map(str, state))
+
+def generate_product(numOfstate: int, size = n):
+    rt = []
+    for x in itertools.product([1, 0], repeat=size):
+        if numOfstate:
+            valueX = evaluateV(x)
+            if valueX == 0:
+                break
+            rt.append(''.join(map(str, x)))
+            numOfstate -= 1
+        else:
+            break
+    return rt
+
+def evaluate(solution: str):
+    totalV = 0
+    totalW = 0
+    no_c = 0
+    set = [0]
+    for i in range(n):
+        if solution[i] == '1':
+            totalV += v[i]
+            totalW += w[i]
+            if c[i] not in set:
+               set.append(c[i])
+    no_c = len(set) - 1
+    if totalW > W:
+        totalV = 0
         no_c = 0
-        set = [0]
-        for i in range(n):
-            if solution[i] == '1':
-                total_value += v[i]
-                total_weight += w[i]
-                if c[i] not in set:
-                    set.append(c[i])
-        no_c = len(set) - 1
-        if total_weight > W:
-            total_value = 0
-            no_c = 0
-            
-        return (no_c, total_value)
+        
+    # return totalV #non class
+    return (no_c, totalV) #class
+
+def generate_neighborhood(beam):
+    neighborhood = []
+    for i in range(n):
+        neighbor = list(beam)
+        neighbor[i] = '1' if neighbor[i] == '0' else '0'
+        neighborhood.append(''.join(neighbor))
+    return neighborhood
     
-    def generate_neighborhood(beam):
+def LocalBeam(k: int, max_iter=10000, generate="random"): 
+    if (generate=="random"):
+        #randomly
+        states = [generate_random(size = n) for _ in range(k)]
+    else:
+        #product
+        states = generate_product(numOfstate=k) # for _ in range(2**n)
+    
+    initial_solution = max(states, key=evaluate)
+    
+    beams = [initial_solution] * k
+    best_solution = max(beams, key=evaluate)
+    best_value = evaluate(best_solution)
+    arr = [0] * n
+
+    for _ in range(max_iter):
         neighborhood = []
-        for i in range(n):
-            neighbor = list(beam)
-            neighbor[i] = '1' if neighbor[i] == '0' else '0'
-            neighborhood.append(''.join(neighbor))
-        return neighborhood
+        for beam in beams:
+            neighborhood += generate_neighborhood(beam)
+        beams = sorted(neighborhood, key=evaluate, reverse=True)[:k]
+        new_best_solution = max(beams, key=evaluate)
+        new_best_value = evaluate(new_best_solution)
+        if new_best_value > best_value:
+            best_solution = new_best_solution
+            best_value = new_best_value
+
+    chosen_items = [i for i in range(n) if best_solution[i] == '1']
+    for item in chosen_items:
+        arr[item] = 1
     
-    def solve(k, max_iterations=100):
-        initial_solution = max([''.join(map(str, x)) for x in itertools.product([0, 1], repeat=n)], key=evaluate)
-        beams = [initial_solution] * k
-        best_solution = max(beams, key=evaluate)
-        best_value = evaluate(best_solution)
-        arr = [0] * n
-
-        for i in range(max_iterations):
-            neighborhood = []
-            for beam in beams:
-                neighborhood += generate_neighborhood(beam)
-            beams = sorted(neighborhood, key=evaluate, reverse=True)[:k]
-            new_best_solution = max(beams, key=evaluate)
-            new_best_value = evaluate(new_best_solution)
-            if new_best_value > best_value:
-                best_solution = new_best_solution
-                best_value = new_best_value
-
-        chosen_items = [i for i in range(n) if best_solution[i] == '1']
-        for item in chosen_items:
-            arr[item] = 1
-        return best_value[1], arr, best_value[0]
+    # return best_value, arr #non_class
+    return best_value[1], arr, best_value[0] #class
     
-    return solve(k, max_inter)
 
-maxTotalV, maxKnapsack, numclass = LocalBeam(k = 2)
+# maxTotalV, maxKnapsack = LocalBeam(k = 2) #nonclass
+
+maxTotalV, maxKnapsack, numclass = LocalBeam(k = 4)
+
 print(maxTotalV)
 print(maxKnapsack)
 print(numclass == m)

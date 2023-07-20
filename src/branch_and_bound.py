6efa26337825fd7c6dde016c430f7f26240a1846
TruongVinhPhuc <139362781+TruongVinhPhuc@users.noreply.github.com>
2023-07-20 19:10:10 +0700

    New branch and bound algorithm

--- src/branch_and_bound.py
+++ src/branch_and_bound.py
@@ -1,44 +1,74 @@
-def knapSack_BNB():
-    bound = [(float(Items_value[i])/float(Items_weight[i])) for i in range(n)]
-    current_weight = 0
-    current_value = 0
-    numbered_items = [i for i in range(n)]
-    sort_based_on_class = [[]]
-    for i in range(Class_number):
-        sort_based_on_class
-        for j in range(n):
-            if Items_class[j] == i + 1:
-                tmp = []
-                tmp.append(float(Items_value[j])/float(Items_weight[j]))
-        tmp.sort(reverse = True)
-        sort_based_on_class.append(tmp)
-    dict = list(zip(bound,numbered_items))
-    dict.sort(reverse = True)        
-    for i in range(Class_number):
-        for j in range(len(sort_based_on_class[i])):
-            index = 0 
-            for (ub,idx) in dict:
-                if sort_based_on_class[i][j] == ub:
-                    index = idx
-                    break
-            take = current_value + Items_value[index] + (Total_weight - current_weight - Items_weight[index])/sort_based_on_class[i][j]
-            not_take = current_value + (Total_weight - current_weight)/sort_based_on_class[i][j]
-            if take >= not_take and current_weight + Items_weight[index] <= Total_weight:
-                current_weight += Items_weight[index]
-                current_value += Items_value[index]
-                selected_items[index] += 1             
-                break               
-    for (i,j) in dict:
-        if selected_items[j] == 0:
-            take = current_value + Items_value[j] + (Total_weight - current_weight - Items_weight[j])/bound[j]
-            not_take = current_value + (Total_weight - current_weight)/bound[j]
-            if take >= not_take and current_weight + Items_weight[j] <= Total_weight:
-                current_weight += Items_weight[j]
-                current_value += Items_value[j]
-                selected_items[j] += 1
-    return current_value             
+def upper_bound(index, current_weight, current_value):
+    remaining_weight = Total_weight - current_weight
+    total_value = current_value
 
-file = open("datasets/INPUT_5.txt")
+    # Sort items in descending order of value-to-weight ratio
+    sorted_indices = sorted(range(index, n), key=lambda i: Items_value[i] / Items_weight[i], reverse=True)
+    for i in sorted_indices:
+        if Items_weight[i] <= remaining_weight:
+            total_value += Items_value[i]
+            remaining_weight -= Items_weight[i]
+        else:
+            total_value += (remaining_weight / Items_weight[i]) * Items_value[i]
+            break
+
+    return total_value
+
+def knapSack_recursive_BNB(current_value, current_weight, index, selected_items, max_value, max_items):
+    if index == n:
+        if current_value > max_value: return current_value, selected_items[:]
+        return max_value, max_items
+
+    # Calculate the bound using fractional knapsack algorithm
+    bound = upper_bound(index,current_weight,current_value)
+    if bound <= max_value:  # Prune the branch if bound is not better than current max_value
+        return max_value, max_items
+
+    # Try not selecting the current item
+    max_value, max_items = knapSack_recursive_BNB(current_value, current_weight, index + 1, selected_items, max_value, max_items)
+    # Try selecting the current item
+    if current_weight + Items_weight[index] <= Total_weight and selected_items[index] == 0:
+        value = Items_value[index]
+        selected_items[index] = selected_items[index] + 1
+        selected_class[Items_class[index] - 1] = 1
+        max_value, max_items = knapSack_recursive_BNB(current_value + value, current_weight + Items_weight[index], index+1, selected_items, max_value, max_items)
+        selected_items[index] = selected_items[index] - 1
+        selected_class[Items_class[index] - 1] = 0
+    if not all(selected_class):
+        for i in range(index, n):
+            class_id = Items_class[i] - 1
+            if not selected_class[class_id]:
+                value = Items_value[i]
+                weight = Items_weight[i]
+                if current_weight + weight <= Total_weight and selected_items[i] == 0:
+                    selected_items[i] = 1
+                    selected_class[class_id] = 1
+                    max_value, max_items = knapSack_recursive_BNB(current_value + value, current_weight + weight, i + 1, selected_items, max_value, max_items)
+                    selected_items[i] = 0
+                    selected_class[class_id] = 0
+                    break        
+    return max_value, max_items
+
+# def KnapSack_select_class_before(Items_value, Items_weight,Items_class, n):
+#     bound = [(float(Items_value[i])/float(Items_weight[i])) for i in range(n)]
+#     current_weight = 0
+#     current_value = 0
+#     numbered_items = [i for i in range(n)]
+#     dict = list(zip(bound,numbered_items))
+#     dict.sort(reverse = True)
+#     for (i,j) in dict:
+#         if Items_class[j] not in selected_class:
+#             selected_class.append(Items_class[j])
+#             current_weight += Items_weight[j]
+#             current_value += Items_value[j]
+#             selected_items[j] += 1
+#     return current_weight,current_value      
+
+# def knapSack():
+#     current_weight, current_value = KnapSack_select_class_before(Items_value,Items_weight,Items_class,n)
+#     return knapSack_recursive_BNB(current_value,current_weight,0,selected_items, max_value, max_items)
+
+file = open("C:/Users/Admin/Desktop/AI/ArtificialIntelligence_21CLC10/datasets/INPUT_2.txt")
 Total_weight = int(file.readline())
 Class_number = int(file.readline())
 Items_weight = file.readline().split(', ')
@@ -48,8 +78,17 @@ Items_value = [int(vi) for vi in Items_value]
 Items_class = file.readline().split(', ')
 Items_class = [int(ci) for ci in Items_class]
 n = len(Items_weight)
+max_value = 0
 selected_class = [0 for i in range(Class_number)]
-#max_items = [0 for i in range(n)]
-selected_items = [0 for i in range(n)]   
-print(knapSack_BNB())
-print(selected_items)
+max_items = [0 for i in range(n)]
+selected_items = [0 for i in range(n)]
+# k = 2
+# file = open("datasets/INPUT_{k}.txt")
+
+max_value, max_items = knapSack_recursive_BNB(0,0,0,selected_items,max_value,max_items)
+
+print("Maximum value:", max_value)
+print("Selected items:")
+for idx, count in enumerate(max_items):
+    if count > 0:
+        print(f"Item {idx} chosen {count} time(s)")
